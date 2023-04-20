03df36fa4a866b98be7343fdb3e05e0acfbba0f5
Denis <mironov_dens@mail.ru>
2023-04-21 01:31:14 +0900

    Add funcs of read - write to file, output all notes, add new note

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
-def main():
+import json
+import datetime
+
+
+def read_notes_file(file_name):  # Функция для чтения списка заметок из файла
+    with open(file_name, 'r') as data:
+        try:
+            notes = json.load(data)
+            return notes
+        except BaseException as e:
+            print('The file contains invalid JSON')
+
+
+def save_notes_json(notes, file_name):  # Функция для сохранения списка заметок в файл
+    with open(file_name, 'w') as f:
+        json.dump(notes, f)
+
+
+def print_notes(notes): # Функция для вывода выбранной записи или всего списка заметок
+    if not notes:
+        print('Заметок не найдено')
+    else:
+        for note in notes:
+            print(f'ID: {note["id"]}')
+            print(f'Заголовок: {note["title"]}')
+            print(f'Тело заметки: {note["body"]}')
+            print(f'Дата/время: {note["timestamp"]}')
+            print('---')        
+
+
+def add_note(notes): # Функция для добавления новой заметки
+    id = len(notes) + 1
+    title = input('Введите заголовок: ')
+    body = input('Введите текст заметки: ')
+    timestamp = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+    new_note = {'id': id, 'title': title, 'body': body, 'timestamp': timestamp}
+    notes.append(new_note)
+    return notes
+
+
+def main():  # Функция главного меню
+
+    file_name = "notes.json"
+    notes = read_notes_file(file_name)
 
     while True:
         print('Выберите действие:')
@@ -12,24 +55,19 @@ def main():
 
         choice = input('Ваш выбор: ')
 
-        if choice == '7':
+        if choice == '1':
+            print_notes(notes)
+        
+        elif choice == '4':
+            notes = add_note(notes)
+            save_notes_json(notes, file_name)
+
+        elif choice == '7':
             break
 
         else:
             print('Неверный выбор пункта меню')
-    
+
+
 if __name__ == '__main__':
     main()
-
-
-'''        elif choice == '2':
-
-        elif choice == '3':
-
-        elif choice == '4':
-
-        elif choice == '5':
-
-        elif choice == '6': 
-
-        elif choice == '7':'''
